630f23f28a0bf16f564a798fdb0987b05dad89a3
Hedi Ben Mustapha <hedi.benmustapha@msb.tn>
2020-05-23 01:27:01 +0100

    "first commit"

---
+++ code/dependencies.R
@@ -0,0 +1,34 @@
+source("./code/functions.R")
+
+list.of.packages <- c(
+  'assertthat',
+  'data.table',
+  'surveyweights',
+  'stringr',
+  'tidyverse',
+  'srvyr',
+  'gganimate',
+  'ggthemes',
+  'plotly',
+  'extrafont',
+  'DT',
+  'magick'
+)
+
+#Install new packages
+install_new_packages(list.of.packages)
+
+library(assertthat)
+library(data.table)
+library(surveyweights)
+library(stringr)
+library(tidyverse)
+library(srvyr)
+library(gganimate)
+library(ggthemes)
+library(plotly)
+library(extrafont)
+library(DT)
+library(magick)
+fonts()
+fonttable()
---
+++ output/report.knit.md
@@ -0,0 +1,46 @@
+---
+title: "Sample R scripts for Consultant Data Analytics, GMDAC Job Offer"
+author: "Hedi Ben Mustapha"
+output: 
+  html_document:
+    toc: true
+    toc_depth: 3
+    toc_float:
+      collapsed: true
+      smooth_scroll: true
+    number_sections: true
+    highlight: null
+    css: style.css
+    df_print: paged
+    mathjax: local
+    self_contained: false
+    
+---
+
+
+
+
+# FOOD SECURITY
+
+
+## FCS (Food Consumption Score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
